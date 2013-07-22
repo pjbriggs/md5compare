a33d0d1e714fdde79919f70b1e5a837c6f34d240
Peter Briggs <peter.briggs@manchester.ac.uk>
2013-07-22 09:59:33 +0100

    Removed .bat files; these are now generated automatically by setup.py during Windows install.

--- compare.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem Batch script to run "compare" Python utility
-setlocal
-call python %USERPROFILE%\Documents\md5compare\compare.py %*
-endlocal
--- go_compare.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem Batch script to launch "go_compare" Python script
-setlocal
-call python %USERPROFILE%\Documents\md5compare\go_compare.py
-endlocal
